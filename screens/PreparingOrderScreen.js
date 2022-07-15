eaa734938c0af61c9536c8b49272941d4086b387
AbdullahGumi <abdulgumi77@gmail.com>
2022-07-15 15:54:10 +0100

    Complete build

---
+++ screens/PreparingOrderScreen.js
@@ -0,0 +1,38 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React, { useEffect } from "react";
+import * as Animatable from "react-native-animatable";
+import * as Progress from "react-native-progress";
+import { useNavigation } from "@react-navigation/native";
+
+const PreparingOrderScreen = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate("Delivery");
+    }, 4000);
+  });
+
+  return (
+    <SafeAreaView className="bg-[#00ccbb] flex-1 justify-center items-center">
+      <Animatable.Image
+        source={require("../assets/Spinner.gif")}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-56 w-6h-56 rounded-lg"
+      />
+
+      <Animatable.Text
+        animation={"slideInUp"}
+        iterationCount={1}
+        className="text-lg my-10 text-white font-bold text-center"
+      >
+        Waiting for Restaurant to accept your order!
+      </Animatable.Text>
+
+      <Progress.Circle size={60} indeterminate color="white" />
+    </SafeAreaView>
+  );
+};
+
+export default PreparingOrderScreen;
